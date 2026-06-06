92f7ef3b975029accfb7d36df0e86e2a9889ff17
Kade R Pitsch <54875162+KadeR-jpg@users.noreply.github.com>
2026-06-06 13:50:55 -0700

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const countmap: Map<number, number> = new Map();
+        for(const n of nums) {
+            let capt = countmap.get(n) ?? 0
+            countmap.set(n, capt + 1);
+        };
+        const sorted = Array.from(countmap.entries()).sort((a, b) => b[1] - a[1]);
+        return sorted.slice(0, k).map(e => e[0]);
+    }
+}
