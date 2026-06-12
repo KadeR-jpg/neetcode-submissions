7f676dc1b6e490704ee972e1a44512b19b1fc6f3
Kade R Pitsch <54875162+KadeR-jpg@users.noreply.github.com>
2026-06-11 21:34:09 -0700

    Add: design-browser-history - submission-3

---
+++ Data Structures & Algorithms/design-browser-history/submission-3.js
@@ -0,0 +1,64 @@
+class HistNode {
+    /**
+     * @constructor
+     * @param {string} val
+     */
+    constructor(val) {
+        this.prev = null;
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class BrowserHistory {
+    /**
+     * @constructor
+     * @param {string} homepage
+     */
+    constructor(homepage) {
+        this.cur = new HistNode(homepage);
+    }
+
+    /**
+     * @param {string} url
+     * @return {void}
+     */
+    visit(url) {
+        const node = new HistNode(url);
+        node.prev = this.cur;
+        this.cur.next = node;
+        this.cur = node;
+    }
+
+    /**
+     * @param {number} steps
+     * @return {string}
+     */
+    back(steps) {
+        while(this.cur.prev !== null && steps > 0) {
+            this.cur = this.cur.prev
+            steps -= 1
+        }
+        return this.cur.val
+    }
+
+    /**
+     * @param {number} steps
+     * @return {string}
+     */
+    forward(steps) {
+        while(this.cur.next !== null && steps > 0) {
+            this.cur = this.cur.next
+            steps -= 1
+        }
+        return this.cur.val
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * var obj = new BrowserHistory(homepage)
+ * obj.visit(url)
+ * var param_2 = obj.back(steps)
+ * var param_3 = obj.forward(steps)
+ */
